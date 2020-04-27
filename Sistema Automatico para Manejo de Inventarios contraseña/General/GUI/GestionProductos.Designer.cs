4ab4d739ed3c8bc480022799bdd22113a4282e1c
kwolf343 <kammonroy@gmail.com>
2020-04-27 00:45:53 -0600

    cambio final

--- Sistema Automatico para Manejo de Inventarios contraseña/General/GUI/GestionProductos.Designer.cs
+++ Sistema Automatico para Manejo de Inventarios contraseña/General/GUI/GestionProductos.Designer.cs
@@ -199,7 +199,7 @@
             // 
             // toolStrip1
             // 
-            this.toolStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.toolStrip1.ImageScalingSize = new System.Drawing.Size(40, 40);
             this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.btnPrincipal,
             this.toolStripSeparator1,
@@ -211,7 +211,7 @@
             this.toolStrip1.LayoutStyle = System.Windows.Forms.ToolStripLayoutStyle.VerticalStackWithOverflow;
             this.toolStrip1.Location = new System.Drawing.Point(0, 0);
             this.toolStrip1.Name = "toolStrip1";
-            this.toolStrip1.Size = new System.Drawing.Size(143, 161);
+            this.toolStrip1.Size = new System.Drawing.Size(143, 297);
             this.toolStrip1.TabIndex = 0;
             this.toolStrip1.Text = "toolStrip1";
             // 
@@ -221,8 +221,9 @@
             this.btnPrincipal.ImageAlign = System.Drawing.ContentAlignment.TopCenter;
             this.btnPrincipal.ImageTransparentColor = System.Drawing.Color.Magenta;
             this.btnPrincipal.Name = "btnPrincipal";
-            this.btnPrincipal.Size = new System.Drawing.Size(141, 24);
+            this.btnPrincipal.Size = new System.Drawing.Size(141, 64);
             this.btnPrincipal.Text = "Principal";
+            this.btnPrincipal.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText;
             // 
             // toolStripSeparator1
             // 
@@ -234,8 +235,9 @@
             this.toolStripButton2.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButton2.Image")));
             this.toolStripButton2.ImageTransparentColor = System.Drawing.Color.Magenta;
             this.toolStripButton2.Name = "toolStripButton2";
-            this.toolStripButton2.Size = new System.Drawing.Size(141, 24);
+            this.toolStripButton2.Size = new System.Drawing.Size(141, 64);
             this.toolStripButton2.Text = "Productos";
+            this.toolStripButton2.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText;
             // 
             // toolStripSeparator2
             // 
@@ -247,8 +249,9 @@
             this.toolStripButton1.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButton1.Image")));
             this.toolStripButton1.ImageTransparentColor = System.Drawing.Color.Magenta;
             this.toolStripButton1.Name = "toolStripButton1";
-            this.toolStripButton1.Size = new System.Drawing.Size(141, 24);
+            this.toolStripButton1.Size = new System.Drawing.Size(141, 64);
             this.toolStripButton1.Text = "Usuarios";
+            this.toolStripButton1.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText;
             // 
             // toolStripSeparator3
             // 
@@ -260,8 +263,9 @@
             this.toolStripButton3.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButton3.Image")));
             this.toolStripButton3.ImageTransparentColor = System.Drawing.Color.Magenta;
             this.toolStripButton3.Name = "toolStripButton3";
-            this.toolStripButton3.Size = new System.Drawing.Size(141, 24);
+            this.toolStripButton3.Size = new System.Drawing.Size(141, 64);
             this.toolStripButton3.Text = "Historial";
+            this.toolStripButton3.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText;
             // 
             // dataGridView1
             // 
@@ -278,7 +282,7 @@
             // 
             // toolStrip2
             // 
-            this.toolStrip2.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.toolStrip2.ImageScalingSize = new System.Drawing.Size(30, 30);
             this.toolStrip2.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.toolStripButton4,
             this.toolStripSeparator4,
