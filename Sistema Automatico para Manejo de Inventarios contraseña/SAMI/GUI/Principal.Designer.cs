e420e2ab14214fb1992f4d9f4a6afc1e14ee3aef
Gerardo Zelaya <57957674+GERARD2619-17@users.noreply.github.com>
2020-04-27 10:56:01 -0600

    solo elimine el boton general de la pantalla principal

--- Sistema Automatico para Manejo de Inventarios contraseña/SAMI/GUI/Principal.Designer.cs
+++ Sistema Automatico para Manejo de Inventarios contraseña/SAMI/GUI/Principal.Designer.cs
@@ -35,12 +35,8 @@
             this.btnRoles = new System.Windows.Forms.Button();
             this.btnUsuarios = new System.Windows.Forms.Button();
             this.lblUsuario = new System.Windows.Forms.Label();
-            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
-            this.generalToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.gestiónInNueToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.button1 = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.dtgInformacion)).BeginInit();
-            this.menuStrip1.SuspendLayout();
             this.SuspendLayout();
             // 
             // dtgInformacion
@@ -130,31 +126,6 @@
             this.lblUsuario.TabIndex = 5;
             this.lblUsuario.Text = "Usuario:";
             // 
-            // menuStrip1
-            // 
-            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
-            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
-            this.generalToolStripMenuItem});
-            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
-            this.menuStrip1.Name = "menuStrip1";
-            this.menuStrip1.Size = new System.Drawing.Size(1068, 28);
-            this.menuStrip1.TabIndex = 6;
-            this.menuStrip1.Text = "menuStrip1";
-            // 
-            // generalToolStripMenuItem
-            // 
-            this.generalToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
-            this.gestiónInNueToolStripMenuItem});
-            this.generalToolStripMenuItem.Name = "generalToolStripMenuItem";
-            this.generalToolStripMenuItem.Size = new System.Drawing.Size(72, 24);
-            this.generalToolStripMenuItem.Text = "General";
-            // 
-            // gestiónInNueToolStripMenuItem
-            // 
-            this.gestiónInNueToolStripMenuItem.Name = "gestiónInNueToolStripMenuItem";
-            this.gestiónInNueToolStripMenuItem.Size = new System.Drawing.Size(173, 26);
-            this.gestiónInNueToolStripMenuItem.Text = "GestiónInNue";
-            // 
             // button1
             // 
             this.button1.Location = new System.Drawing.Point(825, 74);
@@ -177,15 +148,11 @@
             this.Controls.Add(this.btnEmpleados);
             this.Controls.Add(this.texto);
             this.Controls.Add(this.dtgInformacion);
-            this.Controls.Add(this.menuStrip1);
-            this.MainMenuStrip = this.menuStrip1;
             this.Name = "Principal";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Principal";
             this.Load += new System.EventHandler(this.Principal_Load);
             ((System.ComponentModel.ISupportInitialize)(this.dtgInformacion)).EndInit();
-            this.menuStrip1.ResumeLayout(false);
-            this.menuStrip1.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -199,9 +166,6 @@
         private System.Windows.Forms.Button btnRoles;
         private System.Windows.Forms.Button btnUsuarios;
         private System.Windows.Forms.Label lblUsuario;
-        private System.Windows.Forms.MenuStrip menuStrip1;
-        private System.Windows.Forms.ToolStripMenuItem generalToolStripMenuItem;
-        private System.Windows.Forms.ToolStripMenuItem gestiónInNueToolStripMenuItem;
         private System.Windows.Forms.Button button1;
     }
 }
